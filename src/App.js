e169b381d8f5434659863b163b11a7748879d3e9
Akramul Ratul <akramulratul@gmail.com>
2025-06-14 21:25:00 +1000

    backend add

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import { useState } from 'react';
-import Navbar from './components/Navbar';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
 
-const URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/';
+const URL = process.env.REACT_APP_API_URL || "https://json-s3t4.onrender.com";
 
 function App() {
   const [user, setUser] = useState(null);
 
   return (
     <>
-      <Navbar setUser={setUser}/>
-      <div className='container'>
-      {(() => {
-        if (!user) {
-          return <Login url={URL} setUser={setUser} />
-        } else {
-          return (
-            <Routes>
-              <Route path="/" element={<Home url={URL} user={user} />} />
-              <Route path="/login" element={<Login url={URL} setUser={setUser} />} />
-            </Routes>
-          )
-        } 
-      })()}
+      <Navbar setUser={setUser} />
+      <div className="container">
+        {(() => {
+          if (!user) {
+            return <Login url={URL} setUser={setUser} />;
+          } else {
+            return (
+              <Routes>
+                <Route path="/" element={<Home url={URL} user={user} />} />
+                <Route
+                  path="/login"
+                  element={<Login url={URL} setUser={setUser} />}
+                />
+              </Routes>
+            );
+          }
+        })()}
       </div>
     </>
-  )
+  );
 }
 
 export default App;
